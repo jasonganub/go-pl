d67df445ea4ac61b09cc3aa3522630b161d7e9bf
Jason Ganub <jasonganub@gmail.com>
2019-09-17 23:56:09 +0700

    Track filenames that contain duplicates in dup2

--- ch1/dup2.go
+++ ch1/dup2.go
@@ -5,36 +5,49 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	counts := make(map[string]int)
+	dupFiles := make(map[string][]string)
 	files := os.Args[1:]
 
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, "", dupFiles)
 	} else {
-		for _, file := range files {
-			f, err := os.Open(file)
+		for _, filename := range files {
+			f, err := os.Open(filename)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, filename, dupFiles)
 			f.Close()
 		}
 	}
 
 	fmt.Println("Counts")
 	for text, count := range counts {
-		fmt.Printf("%d\t%s\n", text, count)
+		fmt.Printf("%d\t%s\tfiles: %s\n", count, text, strings.Join(dupFiles[text], ","))
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, filename string, dupFiles map[string][]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+
+		containsTextAlready := false
+		for _, filenameInDupFiles := range dupFiles[input.Text()] {
+			if filenameInDupFiles == filename {
+				containsTextAlready = true
+			}
+		}
+
+		if counts[input.Text()] > 1 && !containsTextAlready {
+			dupFiles[input.Text()] = append(dupFiles[input.Text()], filename)
+		}
 	}
 	// Ignoring potential errors from input.Err()
 }
