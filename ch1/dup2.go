d3bd65dfcf009b36dfc39b1eb46f1bd0499ba20e
Jason Ganub <jasonganub@gmail.com>
2019-09-17 22:09:12 +0700

    Add dup2 that can count multiple files or via stdin

---
+++ ch1/dup2.go
@@ -0,0 +1,40 @@
+// Dup1 prints the text of each line that appears more than once from a list of files or via input
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	fmt.Println("Counts")
+	for lines, n := range counts {
+		fmt.Printf("%d\t%s\n", n, lines)
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// Ignoring potential errors from input.Err()
+}
