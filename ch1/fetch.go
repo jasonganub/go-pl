675ca895d1a0a7f74e967a3780800933d3d93003
Jason Ganub <jasonganub@gmail.com>
2019-09-21 19:22:01 +0700

    Add prefix validation on fetch

--- ch1/fetch.go
+++ ch1/fetch.go
@@ -6,10 +6,16 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+
+		if !strings.HasPrefix(url, "http://") {
+			url = fmt.Sprintf("http://%s", url)
+		}
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
